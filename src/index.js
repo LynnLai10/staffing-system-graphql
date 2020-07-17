fde71147a5c8cfac751833428048930224e31ac0
LynnLai10 <52684000+LynnLai10@users.noreply.github.com>
2020-07-17 14:40:35 +1000

    uoplad images to aws s3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import express from 'express'
 import server from "./graphqlServer.js";
+import AWS from "aws-sdk"
 import multer from "multer";
 import sharp from "sharp";
 
-const fs = require("fs");
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ID,
+  secretAccessKey: process.env.AWS_SECRET
+})
 const path = require("path");
-const imgPath = (category) =>
-  path.join(__dirname, `../public/img/${category}`);
 const storage = multer.memoryStorage();
 const upload = multer({
   storage,
@@ -14,8 +15,6 @@ const upload = multer({
     fileSize: 3000000,
   },
 });
-
-server.express.use(express.static('public'))
 
 server.express.all('*', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.CLIENT_ENDPOINT)
@@ -38,19 +37,25 @@ server.express.post(
         fit: sharp.fit.contain,
         background: { r: 255, g: 255, b: 255, alpha: 1 },
       })
-      .toFile(
-        imgPath(req.params.category) +
-          "/" +
-          req.body.fileKeys.split(",")[index] +
-          ".jpg"
-      );
-    res.send();
+      .toBuffer().then(data => {
+        const originalName = req.files[0].originalname.split('.')
+        const fileName = req.body.fileKeys.split(",")[index] + '.' + originalName[originalName.length-1]
+        const params = {
+          Bucket: process.env.AWS_BUCKET_NAME,
+          Key: `${req.params.category}/${fileName}`,
+          Body: data
+        }
+        s3.upload(params, (err, data) => {
+          if (err) {
+            res.status(500).send(err)
+          }
+          res.status(200).send(data)
+        })
+      }).catch(err => {
+        res.status(500).send(err)
+      })
   }
 );
-
-server.express.get('/checkout/:category/:fileName', (req,res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'img', req.params.category, req.params.fileName))
-})
 
 server.express.delete(
   "/checkout/delete/:category/:fileName",
@@ -61,7 +66,7 @@ server.express.delete(
         if (err) throw err;
       }
     );
-    res.send();
+    res.status(200).send();
   }
 );
 
